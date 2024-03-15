9042652c07e504d6b526caef7c44e32b4798af7c
Noah Duggan Erickson <duggann@wwu.edu>
2024-03-15 08:44:19 -0700

    debug

--- data/abide/cnn-model.py
+++ data/abide/cnn-model.py
@@ -19,14 +19,23 @@ for root, dirs, files in os.walk(NIIROOT):
 data = [(p[1][:-12], nib.load(os.path.join(p[0], p[1])).get_fdata()) for p in tqdm(niipaths, desc='load')]
 df = pd.read_csv(CSVPATH)
 
-y = [list(df[df['FILE_ID'] == x[0]]['DX_GROUP'])[0]-1 for x in tqdm(data, desc='pain')]
+y = np.array([[list(df[df['FILE_ID'] == x[0]]['DX_GROUP'])[0]-1 for x in tqdm(data, desc='pain')]]).T
 
-X = [x[1].astype(np.int8) for x in data]
+print(y)
+
+X = [x[1] for x in data]
 
 model = tf.keras.Sequential(name='abide_cnn')
 
 X_tr, X_vl, y_tr, y_vl = train_test_split(X, y, test_size=0.4, random_state=444420)
 X_dv, X_te, y_dv, y_te = train_test_split(X_vl, y_vl, random_state=728153)
+
+X_tr = tf.constant(np.array(X_tr))
+y_tr = tf.constant(np.array(y_tr))
+X_dv = tf.constant(np.array(X_dv))
+y_dv = tf.constant(np.array(y_dv))
+X_te = tf.constant(np.array(X_te))
+y_te = tf.constant(np.array(y_te))
 
 # Model architecture copied from Alharthi & Alzahrani (11/2023)
 # DOI: 10.3390/brainsci13111578
@@ -57,5 +66,6 @@ model.add(tfl.Dense(1, activation='sigmoid'))
 
 model.summary()
 model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['acc', 'f1_score'])
-model.fit(x=X_tr, y=y_tr, epochs=50, batch_size=256, validation_data=(X_dv,y_dv), verbose=1)
-model.save('cnn_1')
+print(type(X_tr))
+model.fit(x=X_tr, y=y_tr, epochs=50, batch_size=1, validation_data=(X_dv,y_dv), verbose=1)
+model.save('cnn_1.keras')
