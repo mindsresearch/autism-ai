1646452d96603e3ba415952622555b46ee53278c
Noah DE <71356818+duggann4@users.noreply.github.com>
2024-05-17 10:12:34 -0700

    hotfix

--- replicating-results/abide/acc_vis.py
+++ replicating-results/abide/acc_vis.py
@@ -6,17 +6,17 @@ from scipy.ndimage import gaussian_filter1d
 
 # Path to csv of accuracies
 csv_path = '/run/media/noah/TOSHIBA EXT/cnn_3-1_accs.csv'
-sigma = 1
+sigma = 0.25
 
 df = pd.read_csv(csv_path)
 df = df.drop(columns=['Raw Train', 'Raw Dev', 'Raw OL'])
 df['outlier'] = df['outlier'].astype(bool)
 df = df.set_index('Epoch')
 
-df['train_raw'] = df['train']
-df['dev_raw'] = df['dev']
-df['train'] = gaussian_filter1d(df['train_raw'], sigma=sigma)
-df['dev'] = gaussian_filter1d(df['dev_raw'], sigma=sigma)
+# df['train_raw'] = df['train']
+# df['dev_raw'] = df['dev']
+# df['train'] = gaussian_filter1d(df['train_raw'], sigma=sigma)
+# df['dev'] = gaussian_filter1d(df['dev_raw'], sigma=sigma)
 
 cm = sns.color_palette('tab20')
 ncm = []
@@ -24,7 +24,6 @@ for i in [1, 3]:
     c = cm[i]
     nc = (c[0], c[1], c[2])
     ncm.append(nc)
-    print(nc)
 
 ndf = df[df['outlier'] == False].drop(columns=['outlier'])
 epsilon = 0.01
@@ -33,7 +32,7 @@ nmax = ndf.max().max() + epsilon
 
 style.use('seaborn-v0_8-talk')
 fig, ax = plt.subplots(figsize=(12, 6))
-sns.lineplot(ndf[['train_raw', 'dev_raw']], dashes=False, ax=ax, palette=ncm, legend=False, linewidth=1)
+# sns.lineplot(ndf[['train_raw', 'dev_raw']], dashes=False, ax=ax, palette=ncm, legend=False, linewidth=1)
 sns.lineplot(df[['train', 'dev']], dashes=False, ax=ax, palette='tab10')
 ax.axhline(y=0.5, color='gray', linestyle='--', linewidth=1)
 ax.set_ylabel('Accuracy')
